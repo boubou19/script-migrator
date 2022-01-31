e5c60fab596d3459646634997b5969a85d746b60
boubou_19 <miisterunknown@gmail.com>
2022-01-31 22:34:28 +0100

    rewrite recipe identification

--- migrator.py
+++ migrator.py
@@ -1,15 +1,23 @@
 from pprint import pprint
 from test import text
-from handlers import handler_assembler, handler_shaped_crafting,\
+from handlers import handler_assembler, handler_shaped_crafting, \
     handler_shapless_crafting, handler_extreme_shaped_crafting
 
 default_strip_list = [" ", "[", "(", "<", ">", ")", "]", ";", "\n"]
 wildcard_value = 32767
 
+handlers = {"addShaped": handler_shaped_crafting,
+            "assembler": handler_assembler,
+            "shapeless": handler_shapless_crafting,
+            "extreme_craft": handler_extreme_shaped_crafting}
+
 
 def process_all(dic):
-    mappings = {"addShaped": handler_shaped_crafting.process, "assembler": handler_assembler.process,
-                "shapeless": handler_shapless_crafting.process, "extreme_craft": handler_extreme_shaped_crafting}
+    mappings = {"addShaped": handler_shaped_crafting.process,
+                "assembler": handler_assembler.process,
+                "shapeless": handler_shapless_crafting.process,
+                "extreme_craft": handler_extreme_shaped_crafting.process}
+
     dic["val"] = [x.strip("val") for x in dic["val"]]
     val_list = sorted(
         [[val[:val.find("=")].replace(" ", ""), val[val.find("=") + 1:].strip(" ").strip(";")] for val in dic["val"]],
@@ -42,121 +50,52 @@ def process_all(dic):
 def main():
     test = text.split("\n")
     recipe = []
-    blacklist = ["recipes.addShaped", "Assembler.addRecipe", "val", "recipes.addShapeless",
-                 "mods.avaritia.ExtremeCrafting.addShaped"]
-    results = {"assembler": [], "val": [], "addShaped": [], "shapeless": [], "extreme_craft": []}
-    assembling = False
-    val = False
-    addShaped = False
-    shapeless = False
-    extreme_craft = False
+    blacklist = {handler_name: handler.discriminator for handler_name, handler in handlers.items()}
+    blacklist["val"] = "val"
+
+    results = {handler_name: [] for handler_name in blacklist.keys()}
+    states = {handler_name: False for handler_name in blacklist.keys()}
+
     added = False
     for line in test:
+        # skip useless lines
         if line == "" or line.startswith("//") or line.startswith("recipes.remove"):
             continue
 
-        # assembler recipe
-        if line.startswith("Assembler.addRecipe"):
-            assembling = True
-            recipe.append(line)
-            added = True
+        for handler_name in results.keys():
 
-        # close assembler recipe
-        if assembling == True and line.endswith(";"):
-            if not added:
+            # assembler recipe
+            if line.startswith(blacklist[handler_name]):
+                states[handler_name] = True
                 recipe.append(line)
-            assembling = False
-            results["assembler"].append("".join(recipe))
-            recipe = []
-            added = False
-            continue
+                added = True
 
-        # val
-        if line.startswith("val"):
-            val = True
-            recipe.append(line)
-            added = True
-
-        # close val
-        if val == True and line.endswith(";"):
-            if not added:
-                recipe.append(line)
-            val = False
-            results["val"].append("".join(recipe))
-            recipe = []
-            added = False
-            continue
-
-        # addshaped recipes
-        if line.startswith("recipes.addShaped"):
-            addShaped = True
-            recipe.append(line)
-            added = True
-
-        # close addshaped recipes
-        if addShaped is True and line.endswith(";"):
-            if not added:
-                recipe.append(line)
-            addShaped = False
-            # print("".join(recipe))
-            results["addShaped"].append("".join(recipe))
-            recipe = []
-            added = False
-            continue
-
-        # addshapeless recipes
-        if line.startswith("recipes.addShapeless"):
-            shapeless = True
-            recipe.append(line)
-            added = True
-
-        # close addshaped recipes
-        if shapeless == True and line.endswith(";"):
-            if not added:
-                recipe.append(line)
-            shapeless = False
-            # print("".join(recipe))
-            results["shapeless"].append("".join(recipe))
-            recipe = []
-            added = False
-            continue
-
-        # avaritia recipes
-        if line.startswith("mods.avaritia.ExtremeCrafting.addShaped"):
-            extreme_craft = True
-            recipe.append(line)
-            added = True
-
-        # close extreme crafting recipes
-        if extreme_craft == True and line.endswith(";"):
-            if not added:
-                recipe.append(line)
-            shapeless = False
-            # print("".join(recipe))
-            results["extreme_craft"].append("".join(recipe))
-            recipe = []
-            added = False
-            continue
+            # close assembler recipe
+            if states[handler_name] is True and line.endswith(";"):
+                if not added:
+                    recipe.append(line)
+                states[handler_name] = False
+                results[handler_name].append("".join(recipe))
+                recipe = []
+                added = False
+                continue
 
         # add line while it's not starting or ending
-        if (val or assembling or addShaped or extreme_craft) and not line.endswith(";") and not all(
-                [x in line for x in blacklist]) \
+        if (any([value for value in states.values()])) and not line.endswith(";") \
+                and not all([x in line for x in blacklist.values()]) \
                 and not added:
             recipe.append(line)
             added = False
             continue
+
         # unknown detection
-        if "recipes" in line and not any([x in line for x in blacklist]):
+        if "recipes" in line and not any([x in line for x in blacklist.values()]):
             print("WARNING: " + line)
 
         added = False
     return results
 
 
-# print(results["addShaped"][10])
-
-# pprint(results["addShaped"])
-# print(prepare_item("gregtech:gt.metaitem.01:32101"))
 if __name__ == "__main__":
     results = main()
     process_all(results)
